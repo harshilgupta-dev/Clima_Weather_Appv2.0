4116d44c5562d52617423fda1bafc646cc12f1b9
Harshil Gupta <harshilgupta0099@gmail.com>
2021-05-23 01:37:08 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+ void main(){
+   runApp(Clima());
+ }
+ 
+ class Clima extends StatelessWidget {
+   const Clima({Key key}) : super(key: key);
+ 
+   @override
+   Widget build(BuildContext context) {
+     return MaterialApp(
+       home: Scaffold(
+       ),
+     );
+   }
+ }
+ 
