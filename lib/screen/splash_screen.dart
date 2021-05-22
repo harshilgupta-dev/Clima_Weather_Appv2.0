eb75f6ed7266e7773df36e950a15f5bc9f0c24d8
Harshil Gupta <harshilgupta0099@gmail.com>
2021-05-23 01:44:58 +0530

    import animated_splash_screen.dart in splash_screen.dart

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 
 class SplashScreen extends StatelessWidget {
-  const SplashScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
